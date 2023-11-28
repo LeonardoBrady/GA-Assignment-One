591100404cc0d05917d942233c13ce839969f74c
Liam <liamleeallen1998@hotmail.com>
2023-11-28 21:15:59 +1030

    Added remove button functionailty and total will update after item is removed.

--- script.js
+++ script.js
@@ -17,6 +17,40 @@ function updateDateTime() {
   setInterval(updateDateTime, 1000);
 
 
+
+
+
+
+
+// This adds functionality to the Remove buttons & updates the carts total.
+const removeCartItemButton = document.getElementsByClassName('remove-item-btn')
+console.log(removeCartItemButton)
+for (let i = 0; i < removeCartItemButton.length; i++) {
+  const button = removeCartItemButton[i];
+  button.addEventListener('click', function(event){
+    const buttonClicked = event.target 
+    buttonClicked.parentElement.remove()
+    updateCartTotal()
+  })
+}
+//Updates carts total
+function updateCartTotal() {
+  const cartItemContainer = document.getElementsByClassName('cart-items')[0]
+  const cartRows = cartItemContainer.getElementsByClassName('cart-item')
+  let total = 0
+  for (let i = 0; i < cartRows.length; i++) {
+    const cartRow = cartRows[i];
+    let priceElement = cartRow.getElementsByClassName('cart-item-total')[0]
+    let quantityElement = cartRow.getElementsByClassName('quantity')[0]
+    let price = parseFloat(priceElement.innerText.replace('Total: $', ''))
+    let quantity = quantityElement.value
+    total = total + (price * quantity)
+  }
+  document.getElementsByClassName('cart-final-total')[0].innerText = 'Cart Total Price: $' + total
+}
+
+
+
   //Variable for CLASS="addToCart"
 let addToCartBTN = document.getElementsByClassName('addToCart')
   console.log(addToCartBTN)
@@ -41,18 +75,26 @@ function addItemToCart(title, price, imageSrc){
   const cartRow = document.createElement('div')
   cartRow.classList.add('cart-item')
   const cartItems = document.getElementsByClassName('cart-items')[0]
-  const cartItemNames = cartItems.getElementsByClassName('')
+  const cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+  for (let i = 0; i < cartItemNames.length; i++) {
+    if(cartItemNames[i].innerText == title){
+      //This loop checks to see if item is already in cart & calls alert()
+      alert('This item is already in the cart')
+      return
+    }
+    
+  }
   let cartRowContents = `
       <img src="${imageSrc}"/>
-      <h2>${title}</h2>
-      <p>Total: ${price}</p>
+      <h2 class="cart-item-title">${title}</h2>
+      <p class="cart-item-total">Total: ${price}</p>
       <!-- cart QTY part -->
       <div class="quantity-control">
         <button class="quantity-btn minus">-</button>
         <input type="number" class="quantity" value="1" min="1" />
         <button class="quantity-btn plus">+</button>
       </div>
-      <button>Remove</button>
+      <button class="remove-item-btn">Remove</button>
   `
   cartRow.innerHTML = cartRowContents
   cartItems.append(cartRow)
