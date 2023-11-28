17b2fc303695f16375ddb0b3429d2d22d2df3776
Liam <liamleeallen1998@hotmail.com>
2023-11-28 16:09:43 +1030

    Added storeProduce in script.js. Also  added class & typeof to store section buttons in html

---
+++ script.js
@@ -0,0 +1,8 @@
+const storeProduce = [
+    { id: 1, name: 'Blueberry', price: 2.50 },
+    { id: 2, name: 'Cherry', price: 105.00 },
+    { id: 3, name: 'Orange', price: 4.50 },
+    { id: 4, name: 'Pineapple', price: 5.50 },
+    { id: 5, name: 'Avocado', price: 3.25 },
+    { id: 6, name: 'Mango', price: 6.75 },
+  ];
