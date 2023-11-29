eaa114135a283c2e3c05252c7325a4445d894d2a
Liam <liamleeallen1998@hotmail.com>
2023-11-29 17:13:09 +1030

    Removed Quantity input from store section

--- script.js
+++ script.js
@@ -208,16 +208,6 @@ function renderShopItems() {
       <br />
       <p class="product-name cart-item-title">${product.name}</p>
       <p><button class="addToCart" type="button">Add to Cart</button></p>
-      <p>
-        QTY:
-        <input
-          class="product-quantity"
-          type="tel"
-          name="quantity"
-          maxlength="1"
-          value="1"
-        />
-      </p>
     `);
 
     // Adds click event listener to the "Add to Cart" button
