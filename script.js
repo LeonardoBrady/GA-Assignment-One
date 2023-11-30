1bb3d2191efb309482d484a0f1dba5113b063296
Liam <liamleeallen1998@hotmail.com>
2023-11-30 23:10:34 +1030

    Moved date and time underneath nav buttons

--- script.js
+++ script.js
@@ -229,9 +229,9 @@ function renderShopItems() {
       // Creates HTML content for the product
       (productDiv.innerHTML = `
       <img class="productImages" src="./images/${product.name}.jpeg"/>
-      <p class="price" style=" background-color: #f0f0f0; color: #333; padding-right: -5px; border-radius: 5px;">$${product.price.toFixed(
-        2
-      )}</p>
+      <p class="price" style=" background-color: #f0f0f0; color: #333; padding-right: -5px; border-radius: 5px;">
+      $${product.price.toFixed(2)}
+      </p>
       <br />
       <p class="product-name cart-item-title">${product.name}</p>
       <p><button class="addToCart" type="button">Add to Cart</button></p>
